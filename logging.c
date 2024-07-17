a9b1d4de86ffa28d1ec3bbf7bf0f4b111913f07a
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2024-07-17 12:52:30 +0200

    CHG logging output

--- logging.c
+++ logging.c
@@ -1,12 +1,24 @@
 #include "logging.h"
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdint.h>
 
-void logging_log_message(const char* message, bool new_line)
+void logging_log_message(const char* message)
 {
     if (strlen(message) > 0)
     {
-        printf(new_line ? "%s\n" : "%s", message);
+        static long first_time_ns = 0.0;
+        clockid_t clockid = 0;
+        struct timespec ts = {0};
+        clock_gettime(0, &ts);
+        int64_t time_ns = ts.tv_sec * 1.0e9 + ts.tv_nsec;
+
+        first_time_ns = first_time_ns == 0.0 ? time_ns : first_time_ns;
+
+        int64_t diff_ns = time_ns - first_time_ns;
+        printf("[%020ld]: %s\n", diff_ns, message);
+
         fflush(stdout);
     }
 }
