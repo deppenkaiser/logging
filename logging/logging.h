ce1cf5f6a43c16b785d2c39bfc82a25751929325
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2026-09-11 15:25:25 +0200

    ...

--- logging/logging.h
+++ logging/logging.h
@@ -6,4 +6,8 @@ void logging_log_message(const char* message);
 void logging_log_formatted(const char* format, ...);
 void logging_log_with_ID(const char* module_id, const char* function, const char* format, ...);
 
-#define LOG(module_id, ...) logging_log_with_ID(module_id, __FUNCTION__, __VA_ARGS__)
+#ifdef LOGGING_ENABLED
+    #define LOG(module_id, ...) logging_log_with_ID(module_id, __FUNCTION__, __VA_ARGS__)
+#else
+    #define LOG(module_id, ...) ((void)0)
+#endif
